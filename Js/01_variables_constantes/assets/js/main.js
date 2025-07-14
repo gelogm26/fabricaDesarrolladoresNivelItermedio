6fc36297cf4de802b9b9085c9e0b4b14ac970576
Angelo gonzalez <4198126@gmail.com>
2025-07-14 11:35:37 -0500

    Tipos de datos y conexión Js desde html

---
+++ Js/01_variables_constantes/assets/js/main.js
@@ -0,0 +1,61 @@
+//Tipos de datos de las variables o constantes
+
+//Tipo string o cadena
+let cadena = "Hola, ¿Cómo estás?"
+console.log(cadena)
+console.log(typeof cadena)
+
+//Tipo númerico (entero)
+let numero = 50;
+console.log(numero)
+console.log(typeof numero)
+
+//Tipo decimal o flotante
+let numeroFloat = 100.25;
+console.log(numeroFloat)
+console.log(typeof numeroFloat)
+
+//Tipo objeto de datos
+let objetoPersona = {
+    nombre: "Angelo",
+    apellido: "gonzalez",
+    celular: 3170560930,
+    email: "4198126@gmail.com",
+    direcciones: {
+        direccion1: "Calle 80",
+        direccion2: "Carrera 14"
+    }
+}
+console.log(objetoPersona)
+console.log(typeof objetoPersona)
+console.log(objetoPersona.direcciones)
+console.log(objetoPersona.direcciones.direccion1)
+
+//Tipo de datos BOOL
+let verdadera = true;
+let falsa = false;
+console.log(verdadera)
+console.log(falsa)
+
+//Tipo funcnión
+function sumar(){
+
+}
+console.log(typeof sumar);
+
+//Tipo undefined
+let undefinida;
+console.log(typeof undefinida);
+
+//tipo null
+let variableNull = null;
+console.log(typeof variableNullnull)
+
+//tipo arreglo
+let arregloNumeros = [1,2,3,4,5,6];
+console.log(arregloNumeros);
+console.log(typeof arregloNumeros);
+
+//tipo cadena vacio
+let cadenaVacia = "";
+console.log(typeof cadenaVacia)
