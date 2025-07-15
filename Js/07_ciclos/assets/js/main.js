1fd6f9a90c68a5f597840c312af7732100b537bb
Angelo gonzalez <4198126@gmail.com>
2025-07-15 08:43:58 -0500

    Ciclo while sencillo

--- Js/07_ciclos/assets/js/main.js
+++ Js/07_ciclos/assets/js/main.js
@@ -1,8 +1,17 @@
 //Ciclo for
 
-console.log("Ciclo for")
+console.log("Ciclo for");
 
 for(let i = 1; i <= 10; i++){
     console.log(`i vale ${i}`)
 };
 
+//Ciclo while
+
+console.log("Ciclo while");
+
+let cont = 1;
+while (cont <= 10) {
+    console.log(`Contador vale: ${cont}`);
+    cont++;
+}
