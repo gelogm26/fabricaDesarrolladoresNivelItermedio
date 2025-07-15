7c09fe34f86dd16fbef6b50efaaafa878e9df57f
Angelo gonzalez <4198126@gmail.com>
2025-07-15 08:57:10 -0500

    Arreglos de una dimensión

---
+++ Js/08_estructura_datos/assets/js/main.js
@@ -0,0 +1,17 @@
+//Arreglos unidimencionales
+
+console.log("<----Arreglos de una dimensión---->");
+
+//Definición
+let arregloNumeros = [];
+console.log(typeof arregloNumeros);
+
+arregloNumeros = [1,2,3,4,5,6,7,8,9,10];
+console.log(arregloNumeros);
+
+console.log("<----Acceder a los datos mediante indexación---->");
+
+console.log(`Primer elemento: ${arregloNumeros[0]}`);
+console.log(`Segundo elemento: ${arregloNumeros[1]}`);
+console.log(`Quinto elemento: ${arregloNumeros[4]}`);
+console.log(`Último elemento: ${arregloNumeros[9]}`);
