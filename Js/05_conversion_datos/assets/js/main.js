14168045970a0acfb86a0b0248f35efb39be5cd5
Angelo gonzalez <4198126@gmail.com>
2025-07-15 08:11:48 -0500

    Conversión tipos de datos comunes

---
+++ Js/05_conversion_datos/assets/js/main.js
@@ -0,0 +1,42 @@
+//Conversión cadena a entero
+
+let numero = '46';
+console.log(typeof numero);
+
+let numeroEntero = parseInt(numero);
+console.log(typeof numeroEntero);
+
+//cadena a flotante
+
+let precioCadena = '19.99';
+console.log(typeof precioCadena);
+
+let precioFlotante = parseFloat(precioCadena);
+console.log(typeof precioFlotante);
+
+//Entero a cadena
+
+let edad = 25;
+console.log(typeof edad);
+
+let edadCadena = edad.toFixed.toString();
+console.log(typeof edadCadena);
+
+//Flotante  a entero
+
+let alturaPersona = 1.75;
+console.log(typeof alturaPersona);
+
+let alturaPersonaFinal = parseInt(alturaPersona);
+console.log(typeof alturaPersonaFinal);
+console.log(alturaPersonaFinal);
+
+//Bool a entero
+
+let Activo = true;
+console.log(typeof Activo);
+
+let ActivoInt = Activo ? 1:0;
+console.log(typeof ActivoInt);
+console.log(ActivoInt);
+
