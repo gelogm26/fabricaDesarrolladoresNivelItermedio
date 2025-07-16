b7e420795cad0dcc5d49d725cf626f46069ffea9
Angelo gonzalez <4198126@gmail.com>
2025-07-15 21:28:22 -0500

    Concepto funciones en Js - Básico

---
+++ Js/09_funciones/assets/js/main.js
@@ -0,0 +1,17 @@
+// function - Palabra reservada para crear la función
+
+//Definición
+function saludar(){
+    console.log("Hola, ¿Cómo estás?");
+    console.log("Qué tengas un excelente día");
+}
+
+//llamar la función
+saludar();
+
+function dibujarTabla5(){
+    for(let i = 1; i <= 10; i++){
+        console.log(`5 * ${i} = ${i*5}`)
+    }
+}
+dibujarTabla5();
